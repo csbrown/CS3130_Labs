cd3202083b27571b44bf90d5d7552ce1dfa51c69
Scott Brown <gitpushoriginmaster@gmail.com>
2022-01-10 15:12:28 -0700

    parallelizing rpubs search to relieve network bottleneck

--- Labs/Lab01_installingR_exploringR/search_rpubs.py
+++ Labs/Lab01_installingR_exploringR/search_rpubs.py
@@ -1,20 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import joblib
+import functools
+import itertools
 
 RPUBS_URL = "https://rpubs.com/latest/{}"
 def search(search_string, depth=100):
     search_string = search_string.lower()
+    results = list(itertools.chain(
+        joblib.Parallel(n_jobs=32)(joblib.delayed(
+            functools.partial(search_page, search_string))(i) 
+            for i in range(1, depth+1))
+    ))
+    return results
+
+def search_page(search_string, i):
+    print("searching page {}".format(i))
     results = []
-    for i in range(1, depth + 1):
-        result = requests.get(RPUBS_URL.format(i))
-        soup = BeautifulSoup(result.text, "html.parser")
-        for pub in soup.find_all(class_="pubtile"):
-            desc = pub.find(class_="desc").decode_contents().lower()
-            link = pub.a["href"]
-            title = pub.find_all("a")[1].decode_contents().lower()
-            if (search_string in desc) or (search_string in title):
-                results.append((title, desc, link))
-                print(results[-1])
+    result = requests.get(RPUBS_URL.format(i))
+    soup = BeautifulSoup(result.text, "html.parser")
+    for pub in soup.find_all(class_="pubtile"):
+        desc = pub.find(class_="desc").decode_contents().lower()
+        link = pub.a["href"]
+        title = pub.find_all("a")[1].decode_contents().lower()
+        if (search_string in desc) or (search_string in title):
+            results.append((title, desc, link))
+            print(results[-1])
     return results
 
 if __name__ == "__main__":
